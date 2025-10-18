c9d0430250622080f525b1fbd589bcf014bf07a7
Herbert Garrido <h.garrido02@ufromail.cl>
2025-10-18 15:09:39 -0300

    home(feat): agregar lista de QuickSuggestionCard en vista home

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ScrollView, StyleSheet, View } from 'react-native';
 import BottomNavigation from '../../components/common/BottomNavigation';
 import HomeHeader from '../../components/home/HomeHeader';
+import QuickSuggestionCard from '../../components/home/QuickSuggestionCard';
 
 const Home = () => {
   const navigationItems = [
@@ -13,8 +14,14 @@ const Home = () => {
 
   return (
     <View style={styles.container}>
-      <ScrollView style={styles.scroll} showsVerticalScrollIndicator={false}>
+      <ScrollView showsVerticalScrollIndicator={false} style={styles.scroll}>
         <HomeHeader />
+
+        <View style={styles.section}>
+          <QuickSuggestionCard title="Pasta con tomate" time="15 min" difficulty="Fácil" ingredients={3} icon="food" />
+          <QuickSuggestionCard title="Ensalada mediterránea" time="10 min" difficulty="Fácil" ingredients={5} icon="food" />
+          <QuickSuggestionCard title="Pollo al limón" time="25 min" difficulty="Intermedio" ingredients={4} icon="food" />
+        </View>
       </ScrollView>
 
       <BottomNavigation items={navigationItems} />
@@ -25,6 +32,7 @@ const Home = () => {
 const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: '#FFF' },
   scroll: { flex: 1 },
+  section: { paddingHorizontal: 10, marginTop: 14 },
 });
 
 export default Home;
