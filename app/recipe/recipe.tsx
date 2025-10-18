12e8e04d802f7829648e5dd4fccefe02cc2c3f56
Herbert Garrido <h.garrido02@ufromail.cl>
2025-10-18 19:36:15 -0300

    recipe(feat): crear componente basico RecipeDetail

---
+++ app/recipe/recipe.tsx
@@ -0,0 +1,28 @@
+import { Feather } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+const RecipeDetail = () => {
+  const router = useRouter();
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.back} onPress={() => router.back()}>
+        <Feather name="arrow-left" size={20} color="#374151" />
+      </TouchableOpacity>
+
+      <View style={styles.center}>
+        <Text style={styles.text}>Detalle de receta</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: '#FFF' },
+  back: { padding: 12, marginTop: 20, marginLeft: 12 },
+  center: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  text: { fontSize: 18, color: '#111827' },
+});
+
+export default RecipeDetail;
